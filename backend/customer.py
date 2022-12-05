1b07313450bbc52815a69acac6dd028517393976
Lior Rabanian <masrab11@gmail.com>
2022-12-06 00:51:28 +0200

    added Customer class

---
+++ backend/customer.py
@@ -0,0 +1,9 @@
+class Customer:
+    def __init__(self, name, phone, address, shipment_date, payment_method) -> None:
+        self.name = name
+        self.phone = phone
+        self.address = address
+        self.shipment_date = shipment_date
+        self.payment_method = payment_method
+        
+    

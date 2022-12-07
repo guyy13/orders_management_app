79b31948e2fe537b02df03fea11af20b017e2a52
Lior Rabanian <masrab11@gmail.com>
2022-12-07 16:53:14 +0200

    added more stuff

---
+++ db/scripts/schema.sql
@@ -0,0 +1,13 @@
+use orders_management;
+
+CREATE TABLE `orders` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `phone` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `shipment_date` datetime NOT NULL,
+  `payment_method` varchar(50) NOT NULL,
+  `paid` varchar(50) NOT NULL,
+  `delivered` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'לא',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
